fb0baecf608b4f0f28eec7f2b8960c3be437d06e
onerdjx <39509372+onerdjx@users.noreply.github.com>
2018-06-08 19:59:56 +0800

    del something

--- apis.py
+++ apis.py
@@ -1,12 +1,1 @@
-print("hello word")
-print("daitoue")
-print("xiediandongxi")
-print("lishishenmolian")
-print("this is a client")
-print("MLGB")
-print("这是一个bug")
-print("Fuck")
-print("what the Funck")
-print("ssssssssshhhhhhhhhhh")
-0987654321
-123456789
+aaaaaaaaaaaa
